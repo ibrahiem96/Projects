305cb97fc124ab0cc9d7fa24a213017db4ccc09a
ibrahiem96 <ibrahiem.mohammed96@gmail.com>
2016-05-09 19:18:25 -0500

    Add files via upload

---
+++ CS 341 - Project4 [Windchill Calculation using F#]/Windchills/Function.fs
@@ -0,0 +1,18 @@
+﻿//
+// F# program to compute windchill
+//
+//Ibrahiem Mohammad
+//University of Illinois
+//CS341, Spring 2016
+//HW 4
+//
+
+#light
+
+module Function
+
+
+let windchill T W =
+  //let T = 20.0
+  let WC = 35.7 + 0.6*(T) - 35.7*System.Math.Pow(W, 0.16) + 0.43*(T)*System.Math.Pow(W, 0.16)
+  WC
